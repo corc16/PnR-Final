aaa169039b13d7069434fc3c7aafa2184b85e45b
CorcoranO <corcorano@gilmour.org>
2017-09-29 14:39:07 -0400

    updated dance function

--- student.py
+++ student.py
@@ -57,7 +57,7 @@ class Piggy(pigo.Pigo):
         """executes a series of methods that add up to a compound dance"""
         print("\n---- LET'S DANCE ----\n")
         ##### WRITE YOUR FIRST PROJECT HERE
-        self.encR(1)
+        self.encR(5)
 
     def right(self):
         """subroutine of dance method"""
