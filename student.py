03481405dca047eeeb2bfe02dee80f2c668a49fd
CorcoranO <corcorano@gilmour.org>
2017-10-04 14:51:24 -0400

    updated dance routine

--- student.py
+++ student.py
@@ -86,8 +86,8 @@ class Piggy(pigo.Pigo):
         self.encR(5)
         self.encF(10)
         for x in range(3):
-            self.encR(8)
-            self.encL(9)
+            self.encR(20)
+            self.encL(21)
 
     def shooting_stars(self):
         for x in range(7):
